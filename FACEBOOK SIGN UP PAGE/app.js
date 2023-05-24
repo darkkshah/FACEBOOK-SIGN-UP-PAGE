12cb89d09154ed52f8ce6bbce38ee198812f69de
darkkshah <128061778+darkkshah@users.noreply.github.com>
2023-05-24 15:20:35 +0500

    Add files via upload

---
+++ FACEBOOK SIGN UP PAGE/app.js
@@ -0,0 +1,26 @@
+var inp = document.getElementById("fname")
+
+
+var inp2 = document.getElementById("lname")
+
+var inp3 = document.getElementById("email")
+
+var inp4 = document.getElementById("password")
+
+var inp5 = document.getElementById("dob")
+
+var disp = document.getElementById("male")
+
+var disp2 = document.getElementById("female")
+
+function register () {
+    console.log(inp.value)
+    console.log(inp2.value)
+    console.log(inp3.value)
+    console.log(inp4.value)
+    console.log(inp5.value)
+    console.log(disp.value)
+    console.log(disp2.value)
+    
+}
+
